aaf9d09f33fbe635846769bc33ee1d3ccd3ad1e3
Ubuntu <ubuntu@ip-172-31-45-22.ap-south-1.compute.internal>
2024-06-14 08:22:56 +0000

    tomcat code

---
+++ src/main/java/simplilearngrp/simpliearnarfact/SimpliearnarfactApplication.java
@@ -0,0 +1,13 @@
+package simplilearngrp.simpliearnarfact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpliearnarfactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpliearnarfactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simplilearngrp/simpliearnarfact/SimpliearnarfactApplicationTests.java
@@ -0,0 +1,13 @@
+package simplilearngrp.simpliearnarfact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpliearnarfactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
